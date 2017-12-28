0dd6b736b58cb8c927c6c868655e76fb7f64671f
Simon Bakkevig <simon.nandi@gmail.com>
2017-12-28 21:42:38 +0100

    Day 3

--- src/main/kotlin/com/headlessideas/adventofcode/december3/DataAccess.kt
+++ src/main/kotlin/com/headlessideas/adventofcode/december3/DataAccess.kt
@@ -1,20 +1,21 @@
 package com.headlessideas.adventofcode.december3
 
+import kotlin.math.abs
+import kotlin.math.ceil
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-fun getDistance(start: Int): Int {
+fun getDistance(input: Int): Int {
 
-    println(start - sqrt(start.toFloat()).toInt().toFloat().pow(2))
-
-    val ceilings = generateSequence(1) { ((it + 1) / 2 + 1) * 2 - 1 }
-
-    val layer = ceilings.takeWhile { it.toDouble().pow(2) < start }.last() + 2
-    println(layer)
-    return (layer + 1) / 2
+    val circle = ceil(sqrt(input.toFloat()) / 2)
+    val circleZero = (circle * 2 - 1).pow(2)
+    val centers = (1..7 step 2).map { circleZero + it * circle }
+    val distance = circle + centers.map { abs(input - it) }.min()!!
+    return distance.toInt()
 }
 
 fun main(args: Array<String>) {
     val input = 289326
     println(getDistance(input))
+    //No part 2 for me
 }
